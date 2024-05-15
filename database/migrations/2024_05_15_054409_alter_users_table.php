ff0b0f50ca2cecf90c19195b6daf1010209353ac
Nripesh <nripeshparajuli2@gmail.com>
2024-05-15 12:45:50 +0545

    new commit

---
+++ database/migrations/2024_05_15_054409_alter_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('image')->after('password');
+            $table->foreignId('parent_id')->references('id')->on('users')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('image');
+            $table->dropForeign(['parent_id']);
+            $table->dropColumn('parent_id');
+        });
+    }
+};
